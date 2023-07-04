f75dee94d969aba2fbc8b0d43fc24cfc39304da7
Miqdad <miqdaddeveloper@gmail.com>
2023-07-04 09:08:30 +0700

    (feat) : Add Article Repository to connect domain layer to data layer

---
+++ lib/features/daily_news/domain/repository/article_repository.dart
@@ -0,0 +1,7 @@
+import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
+import '../../../../core/resources/data_state.dart';
+
+abstract class ArticleRepository{
+
+  Future<DataState<List<ArticleEntity>>> getNewsArticle();
+}
