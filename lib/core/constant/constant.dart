8a7ca190026ca1636ad08584a69237707c486a51
Miqdad <miqdaddeveloper@gmail.com>
2023-07-05 08:20:35 +0700

    (feat) : Add newsApiKey, countryQuery & categoryQuery constant string

--- lib/core/constant/constant.dart
+++ lib/core/constant/constant.dart
@@ -1,1 +1,4 @@
 const String newsAPIBaseUrl = 'https://newsapi.org/v2';
+const String newsAPIKey = '0d867713f8d94bbb9f6be8d3d1181043';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
