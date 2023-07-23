30672a45d0892af5d26add1129a524841741c44e
Miqdad <miqdaddeveloper@gmail.com>
2023-07-23 22:20:21 +0700

    (fix & feat) : Change Model use toEntity and remove ArticleEntity extend of ArticleModel

--- lib/features/daily_news/data/models/article.dart
+++ lib/features/daily_news/data/models/article.dart
@@ -1,19 +1,29 @@
 import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
 
-class ArticleModel extends ArticleEntity {
-  const ArticleModel({
-    int? id,
-    String? author,
-    String? title,
-    String? description,
-    String? url,
-    String? urlToImage,
-    String? publishedAt,
-    String? content,
+class ArticleModel{
+  ArticleModel({
+    this.id,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
   });
+
+  final int? id;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String? publishedAt;
+  final String? content;
 
   factory ArticleModel.fromJson(Map<String, dynamic> map) {
     return ArticleModel(
+      id: map['id'] ?? 0,
       author: map['author'] ?? '',
       title: map['title'] ?? '',
       description: map['description'] ?? '',
@@ -23,4 +33,43 @@ class ArticleModel extends ArticleEntity {
       content: map['content'] ?? '',
     );
   }
+
+  ArticleEntity toEntity() {
+    return ArticleEntity(
+      author: author,
+      title: title,
+      description: description,
+      url: url,
+      urlToImage: urlToImage,
+      publishedAt: publishedAt,
+      content: content,
+    );
+  }
 }
+
+// import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
+
+// class ArticleModel extends ArticleEntity {
+//   const ArticleModel({
+//     int? id,
+//     String? author,
+//     String? title,
+//     String? description,
+//     String? url,
+//     String? urlToImage,
+//     String? publishedAt,
+//     String? content,
+//   });
+//
+//   factory ArticleModel.fromJson(Map<String, dynamic> map) {
+//     return ArticleModel(
+//       author: map['author'] ?? '',
+//       title: map['title'] ?? '',
+//       description: map['description'] ?? '',
+//       url: map['url'] ?? '',
+//       urlToImage: map['urlToImage'] ?? '',
+//       publishedAt: map['publishedAt'] ?? '',
+//       content: map['content'] ?? '',
+//     );
+//   }
+// }
