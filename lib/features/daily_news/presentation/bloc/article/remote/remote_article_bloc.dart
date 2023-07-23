7e602e829a0ff7ee6dbd7786d3261a95e3398570
Miqdad <miqdaddeveloper@gmail.com>
2023-07-23 14:09:40 +0700

    (fix & feat) : Add  onGetArticle and return data from usecase into state

--- lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -2,16 +2,30 @@ import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:news_app_clean_architecture/core/resources/data_state.dart';
 
 import '../../../../domain/entities/article.dart';
-
+import '../../../../domain/usecase/get_article.dart';
 
 part 'remote_article_event.dart';
+
 part 'remote_article_state.dart';
 
-class RemoteArticleBloc extends Bloc<RemoteArticleEvent, RemoteArticleState> {
-  RemoteArticleBloc() : super(RemoteArticleInitial()) {
-    on<RemoteArticleEvent>((event, emit) {
-    });
+class RemoteArticlesBloc
+    extends Bloc<RemoteArticlesEvent, RemoteArticlesState> {
+  final GetArticlesUseCase _getArticlesUseCase;
+
+  RemoteArticlesBloc(this._getArticlesUseCase)
+      : super(const RemoteArticleLoading()) {
+    on<GetArticles>(onGetArticles);
   }
+
+  void onGetArticles(
+      GetArticles event, Emitter<RemoteArticlesState> emit) async {
+    final dataState = await _getArticlesUseCase();
+    if (dataState is DataSuccess && dataState.data!.isNotEmpty) {
+      emit(RemoteArticleSuccess(dataState.data!));
+    }
+  }
+
 }
