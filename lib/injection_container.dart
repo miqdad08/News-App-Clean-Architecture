adce1c3ed3c25b8a25ab5d28f0196da521266bef
Miqdad <miqdaddeveloper@gmail.com>
2023-07-23 17:03:18 +0700

    (fix & feat) : Fix useCase Regist in initializeDependencies

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-import 'package:news_app_clean_architecture/core/usecases/usecase.dart';
 import 'package:news_app_clean_architecture/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:news_app_clean_architecture/features/daily_news/data/repository/article_repository_impl.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
@@ -20,7 +19,7 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<ArticleRepository>(ArticleRepositoryImpl(sl()));
 
   //UseCase
-  sl.registerSingleton<UseCase>(GetArticlesUseCase(sl()));
+  sl.registerSingleton<GetArticlesUseCase>(GetArticlesUseCase(sl()));
 
   //Blocs
   sl.registerFactory<RemoteArticlesBloc>(() => RemoteArticlesBloc(sl()));
