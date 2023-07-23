89c81f54c6a9d4a07162bacede2e20937f0f25d2
Miqdad <miqdaddeveloper@gmail.com>
2023-07-23 17:04:30 +0700

    (fix & feat) : Add Body only for test

--- lib/features/daily_news/presentation/pages/home/daily_news.dart
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
 
 class DailyNews extends StatelessWidget {
   const DailyNews({Key? key}) : super(key: key);
@@ -7,6 +10,7 @@ class DailyNews extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppbar(),
+      body: _buildBody() ,
     );
   }
 
@@ -18,6 +22,34 @@ class DailyNews extends StatelessWidget {
           color: Colors.black,
         ),
       ),
+    );
+  }
+
+  _buildBody() {
+    return BlocBuilder<RemoteArticlesBloc, RemoteArticlesState>(
+      builder: (_, state) {
+        if (state is RemoteArticleLoading) {
+          return const Center(
+            child: CupertinoActivityIndicator(),
+          );
+        }
+        if (state is RemoteArticleFailed) {
+          return const Center(
+            child: Icon(Icons.refresh),
+          );
+        }
+        if (state is RemoteArticleSuccess) {
+          return ListView.builder(
+            itemCount: state.articles!.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('asd$index'),
+              );
+            },
+          );
+        }
+        return const SizedBox();
+      },
     );
   }
 }
