3cfa2f396187adf0ed5009f744d5e0a20f093c1a
Miqdad <miqdaddeveloper@gmail.com>
2023-07-23 14:33:15 +0700

    (fix & feat) : Add DailyNews StatelessClass

---
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DailyNews extends StatelessWidget {
+  const DailyNews({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
