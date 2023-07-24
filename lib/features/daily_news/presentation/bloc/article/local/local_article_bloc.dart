a031731d06b53dc8a9be99ed052e6196ec0dbf25
Miqdad <miqdaddeveloper@gmail.com>
2023-07-24 22:59:07 +0700

    (fix & feat) : Add onGetSaveArticles, onRemoveArticle, onSavedArticle into bloc

--- lib/features/daily_news/presentation/bloc/article/local/local_article_bloc.dart
+++ lib/features/daily_news/presentation/bloc/article/local/local_article_bloc.dart
@@ -4,6 +4,7 @@ import 'package:meta/meta.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/usecase/get_saved_article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/usecase/save_article.dart';
 
+import '../../../../domain/entities/article.dart';
 import '../../../../domain/usecase/remove_article.dart';
 
 part 'local_article_event.dart';
@@ -19,9 +20,29 @@ class LocalArticleBloc extends Bloc<LocalArticlesEvent, LocalArticlesState> {
     this._getSavedArticleUseCase,
     this._saveArticleUseCase,
     this._removeArticleUseCase,
-  ) : super(LocalArticleInitial()) {
-    on<LocalArticlesEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  ) : super(const LocalArticleLoading()) {
+    on<GetSavedArticles>(onGetSaveArticles);
+    on<RemoveArticle>(onRemoveArticle);
+    on<SaveArticle>(onSavedArticle);
+  }
+
+  void onGetSaveArticles(
+      GetSavedArticles event, Emitter<LocalArticlesState> emit) async {
+    final articles = await _getSavedArticleUseCase();
+    emit(LocalArticleSuccess(articles));
+  }
+
+  void onRemoveArticle(
+      RemoveArticle removeArticle, Emitter<LocalArticlesState> emit) async {
+    await _removeArticleUseCase(params: removeArticle.article);
+    final articles = await _getSavedArticleUseCase();
+    emit(LocalArticleSuccess(articles));
+  }
+
+  void onSavedArticle(
+      SaveArticle saveArticle, Emitter<LocalArticlesState> emit) async {
+    await _saveArticleUseCase(params: saveArticle.article);
+    final articles = await _getSavedArticleUseCase();
+    emit(LocalArticleSuccess(articles));
   }
 }
