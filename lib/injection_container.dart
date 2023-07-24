252297cf536efbfb58c6864a119cf3c9f066d392
Miqdad <miqdaddeveloper@gmail.com>
2023-07-24 21:25:18 +0700

    (fix & feat) : Define and build the database in initializeDependencies

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:news_app_clean_architecture/features/daily_news/data/data_sources/local/app_database.dart';
 import 'package:news_app_clean_architecture/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:news_app_clean_architecture/features/daily_news/data/repository/article_repository_impl.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
@@ -9,6 +10,10 @@ import 'package:news_app_clean_architecture/features/daily_news/presentation/blo
 final sl = GetIt.instance;
 
 Future<void> initializeDependencies() async {
+
+  final database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+  sl.registerSingleton<AppDatabase>(database);
+
   // Dio
   sl.registerSingleton<Dio>(Dio());
 
