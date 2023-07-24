6da599781636b886d81c5e3bf2215891d341699e
Miqdad <miqdaddeveloper@gmail.com>
2023-07-24 22:02:44 +0700

    (fix & feat) : return from articleDao to repository

--- lib/features/daily_news/data/repository/article_repository_impl.dart
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -4,13 +4,17 @@ import 'package:dio/dio.dart';
 import 'package:news_app_clean_architecture/core/constant/constant.dart';
 import 'package:news_app_clean_architecture/core/resources/data_state.dart';
 import 'package:news_app_clean_architecture/features/daily_news/data/data_sources/remote/news_api_service.dart';
+import 'package:news_app_clean_architecture/features/daily_news/data/models/article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
 
+import '../data_sources/local/app_database.dart';
+
 class ArticleRepositoryImpl implements ArticleRepository {
   final NewsApiService _newsApiService;
+  final AppDatabase _appDatabase;
 
-  ArticleRepositoryImpl(this._newsApiService);
+  ArticleRepositoryImpl(this._newsApiService, this._appDatabase);
 
   @override
   Future<DataState<List<ArticleEntity>>> getNewsArticles() async {
@@ -39,20 +43,18 @@ class ArticleRepositoryImpl implements ArticleRepository {
   }
 
   @override
-  Future<void> deleteArticle(ArticleEntity articleEntity) {
-    // TODO: implement deleteArticle
-    throw UnimplementedError();
+  Future<List<ArticleEntity>> getSavedArticle() async {
+    final savedArticle = await _appDatabase.articleDao.getArticles();
+    return savedArticle.map((model) => model.toEntity()).toList();
   }
 
   @override
-  Future<List<ArticleEntity>> getSavedArticle() {
-    // TODO: implement getSavedArticle
-    throw UnimplementedError();
+  Future<void> removeArticle(ArticleEntity article) {
+    return _appDatabase.articleDao.deleteArticle(ArticleModel.fromEntity(article));
   }
 
   @override
   Future<void> saveArticle(ArticleEntity article) {
-    // TODO: implement saveArticle
-    throw UnimplementedError();
+    return _appDatabase.articleDao.insertArticle(ArticleModel.fromEntity(article));
   }
 }
