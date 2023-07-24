4d0d173fa46d19ff9828b8cf4953a1e17f79f579
Miqdad <miqdaddeveloper@gmail.com>
2023-07-24 22:17:25 +0700

    (fix & feat) : Rename with UseCase lastName class

--- lib/features/daily_news/domain/usecase/remove_article.dart
+++ lib/features/daily_news/domain/usecase/remove_article.dart
@@ -2,11 +2,11 @@ import 'package:news_app_clean_architecture/core/usecases/usecase.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
 
-class RemoveArticle implements UseCase<void, ArticleEntity>{
+class RemoveArticleUseCase implements UseCase<void, ArticleEntity>{
 
   final ArticleRepository _articleRepository;
 
-  RemoveArticle(this._articleRepository);
+  RemoveArticleUseCase(this._articleRepository);
 
   @override
   Future<void> call({ArticleEntity? params}) {
--- lib/features/daily_news/domain/usecase/save_article.dart
+++ lib/features/daily_news/domain/usecase/save_article.dart
@@ -2,10 +2,10 @@ import 'package:news_app_clean_architecture/core/usecases/usecase.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
 
-class SaveArticle implements UseCase<void, ArticleEntity> {
+class SaveArticleUseCase implements UseCase<void, ArticleEntity> {
   final ArticleRepository _articleRepository;
 
-  SaveArticle(this._articleRepository);
+  SaveArticleUseCase(this._articleRepository);
 
   @override
   Future<void> call({ArticleEntity? params}) {
