1067150516977b3bb7c3a3861287f87ad2adf007
Miqdad <miqdaddeveloper@gmail.com>
2023-07-24 22:30:39 +0700

    (fix & feat) : Regist GetSavedArticleUseCase, SaveArticleUseCase, RemoveArticleUseCase into di

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -5,6 +5,9 @@ import 'package:news_app_clean_architecture/features/daily_news/data/data_source
 import 'package:news_app_clean_architecture/features/daily_news/data/repository/article_repository_impl.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/usecase/get_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecase/get_saved_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecase/remove_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecase/save_article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
 
 final sl = GetIt.instance;
@@ -21,10 +24,16 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<NewsApiService>(NewsApiService(sl()));
 
   //Repository
-  sl.registerSingleton<ArticleRepository>(ArticleRepositoryImpl(sl()));
+  sl.registerSingleton<ArticleRepository>(ArticleRepositoryImpl(sl(), sl()));
 
   //UseCase
   sl.registerSingleton<GetArticlesUseCase>(GetArticlesUseCase(sl()));
+
+  sl.registerSingleton<GetSavedArticleUseCase>(GetSavedArticleUseCase(sl()));
+
+  sl.registerSingleton<SaveArticleUseCase>(SaveArticleUseCase(sl()));
+
+  sl.registerSingleton<RemoveArticleUseCase>(RemoveArticleUseCase(sl()));
 
   //Blocs
   sl.registerFactory<RemoteArticlesBloc>(() => RemoteArticlesBloc(sl()));
