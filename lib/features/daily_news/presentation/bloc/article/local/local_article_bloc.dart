24551881fb52959ed951f008de055aba51965182
Miqdad <miqdaddeveloper@gmail.com>
2023-07-24 22:46:46 +0700

    (fix & feat) : initialize LocalUseCase

--- lib/features/daily_news/presentation/bloc/article/local/local_article_bloc.dart
+++ lib/features/daily_news/presentation/bloc/article/local/local_article_bloc.dart
@@ -1,13 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
-import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecase/get_saved_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecase/save_article.dart';
+
+import '../../../../domain/usecase/remove_article.dart';
 
 part 'local_article_event.dart';
+
 part 'local_article_state.dart';
 
 class LocalArticleBloc extends Bloc<LocalArticlesEvent, LocalArticlesState> {
-  LocalArticleBloc() : super(LocalArticleInitial()) {
+  final GetSavedArticleUseCase _getSavedArticleUseCase;
+  final SaveArticleUseCase _saveArticleUseCase;
+  final RemoveArticleUseCase _removeArticleUseCase;
+
+  LocalArticleBloc(
+    this._getSavedArticleUseCase,
+    this._saveArticleUseCase,
+    this._removeArticleUseCase,
+  ) : super(LocalArticleInitial()) {
     on<LocalArticlesEvent>((event, emit) {
       // TODO: implement event handler
     });
