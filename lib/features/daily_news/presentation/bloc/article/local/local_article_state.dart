5ac0cac98e71c8cdef68173e66f7c539d2e9ecd5
Miqdad <miqdaddeveloper@gmail.com>
2023-07-24 22:43:56 +0700

    (fix & feat) : Add LocalArticleLoading & LocalArticleSuccess State

--- lib/features/daily_news/presentation/bloc/article/local/local_article_state.dart
+++ lib/features/daily_news/presentation/bloc/article/local/local_article_state.dart
@@ -1,6 +1,21 @@
 part of 'local_article_bloc.dart';
 
 @immutable
-abstract class LocalArticleState {}
+abstract class LocalArticleState extends Equatable{
+  final List<ArticleEntity> ? articles;
+
+  const LocalArticleState({this.articles});
+
+  @override
+  List<Object?> get props => [articles!];
+}
 
 class LocalArticleInitial extends LocalArticleState {}
+
+class LocalArticleLoading extends LocalArticleState{
+  const LocalArticleLoading();
+}
+
+class LocalArticleSuccess extends LocalArticleState{
+  const LocalArticleSuccess(List<ArticleEntity> articles) : super(articles: articles);
+}
