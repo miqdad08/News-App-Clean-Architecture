64b76201576f5fdb5a1237f09a2b1704b00672f7
Miqdad <miqdaddeveloper@gmail.com>
2023-07-24 22:35:18 +0700

    (fix & feat) : Add ArticleEntity for parameter in LocalArticleEvent

--- lib/features/daily_news/presentation/bloc/article/local/local_article_bloc.dart
+++ lib/features/daily_news/presentation/bloc/article/local/local_article_bloc.dart
@@ -1,7 +1,7 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
 
 part 'local_article_event.dart';
 part 'local_article_state.dart';
--- lib/features/daily_news/presentation/bloc/article/local/local_article_event.dart
+++ lib/features/daily_news/presentation/bloc/article/local/local_article_event.dart
@@ -1,4 +1,11 @@
 part of 'local_article_bloc.dart';
 
 @immutable
-abstract class LocalArticleEvent {}
+abstract class LocalArticleEvent extends Equatable {
+  final ArticleEntity? article;
+
+  const LocalArticleEvent(this.article);
+
+  @override
+  List<Object?> get props => [article];
+}
