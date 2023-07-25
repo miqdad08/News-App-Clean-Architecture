9f89dceac59c612b125af091171aa3b35d53fee5
Miqdad <miqdaddeveloper@gmail.com>
2023-07-25 08:50:23 +0700

    (fix & feat) : Add Article Detail,add Floating Action button, add Body Article

---
+++ lib/features/daily_news/presentation/pages/home/article_detail.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/local/local_article_bloc.dart';
+
+import '../../../../../injection_container.dart';
+
+class ArticleDetail extends StatelessWidget {
+  static const String routeName = '/article-detai';
+  final ArticleEntity? article;
+
+  const ArticleDetail({Key? key, this.article}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => sl<LocalArticlesBloc>(),
+      child: Scaffold(
+        body: _buildBody(),
+        floatingActionButton: _buildFloatingActionButton(),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          _buildArticleTitleAndDate(),
+          _buildArticleImage(),
+          _buildArticleDescription()
+        ],
+      ),
+    );
+  }
+
+  Widget _buildArticleTitleAndDate() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 22),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Title
+          Text(
+            article!.title!,
+            style: const TextStyle(
+                fontFamily: 'Butler',
+                fontSize: 20,
+                fontWeight: FontWeight.w900),
+          ),
+
+          const SizedBox(height: 14),
+          // DateTime
+          Row(
+            children: [
+              const Icon(CupertinoIcons.time, size: 16),
+              const SizedBox(width: 4),
+              Text(
+                article!.publishedAt!,
+                style: const TextStyle(fontSize: 12),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildArticleImage() {
+    return Container(
+      width: double.maxFinite,
+      height: 250,
+      margin: const EdgeInsets.only(top: 14),
+      child: Image.network(
+        article!.urlToImage!,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _buildArticleDescription() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 18),
+      child: Text(
+        '${article!.description ?? ''}\n\n${article!.content ?? ''}',
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  Widget _buildFloatingActionButton() {
+    return Builder(
+      builder: (BuildContext context) => FloatingActionButton(
+        onPressed: () => _onFloatingActionButtonPressed(context),
+        child: const Icon(
+          CupertinoIcons.bookmark,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  void _onFloatingActionButtonPressed(BuildContext context) {
+    BlocProvider.of<LocalArticlesBloc>(context).add(SaveArticle(article!));
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        backgroundColor: Colors.black,
+        content: Text('Article saved successfully.'),
+      ),
+    );
+  }
+}
