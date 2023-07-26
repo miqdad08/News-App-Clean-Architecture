b94904c63a678eb55c41146731065f21568a70ca
Miqdad <miqdaddeveloper@gmail.com>
2023-07-26 21:06:01 +0700

    (fix & feat) : YOUR API KEY INFORMATION

--- lib/core/constant/constant.dart
+++ lib/core/constant/constant.dart
@@ -1,4 +1,4 @@
 const String newsAPIBaseUrl = 'https://newsapi.org/v2';
-const String newsAPIKey = '0d867713f8d94bbb9f6be8d3d1181043';
+const String newsAPIKey = 'YOUR_API_KEY';
 const String countryQuery = 'us';
 const String categoryQuery = 'general';
